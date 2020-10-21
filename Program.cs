e8747c9e7ad72d302054b94cdc2110b90bdbeb40
Flávio José Formis <flavio.formis@pca.com.br>
2020-10-21 15:41:20 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IndexesRanges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] intArray = new int[]{0,1,2,3,4,5,6,7,8,9};
+
+            int[] parcial = intArray[2..6];
+
+            foreach(var i in parcial){
+                Console.WriteLine(i);
+            }
+
+            for (int i =1; i <= parcial.Length; i++){
+                Console.WriteLine(parcial[^i]);
+            }
+
+        }
+    }
+    
+}
